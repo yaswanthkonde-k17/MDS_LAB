0ae6f964a9e3a305d08010beabc0725d32952862
yaswanthkonde-k17 <yaswanthkonde90@gmail.com>
2026-07-07 05:48:16 +0000

    Add code1.py and code2.py examples

---
+++ code1.py
@@ -0,0 +1,1 @@
+print('This is code from code1.py')\ndef multiply(a, b):\n    return a * b\nprint(f'5 * 3 = {multiply(5, 3)}')
---
+++ code2.py
@@ -0,0 +1,1 @@
+import math\nprint('This is code from code2.py')\ndef get_sqrt(x):\n    return math.sqrt(x)\nprint(f'Square root of 16 = {get_sqrt(16)}')
